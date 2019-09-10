6caaa38aa3fa431f90ad49943cdebc8a36a5a0f4
niluiz <nicoleduarteluna@hotmail.com>
2019-09-09 22:28:00 -0300

    Exercício Intent

---
+++ app/src/main/java/br/com/etechoracio/cadastro/AdicionalActivity.java
@@ -0,0 +1,13 @@
+package br.com.etechoracio.cadastro;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AdicionalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adicional);
+    }
+}
